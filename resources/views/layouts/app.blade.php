c05f91397bee71a99c452ca5e4d4e46d1525e904
Filippo Romeo <romeofilippo95@gmail.com>
2020-06-15 16:20:24 +0100

    File upload fixed, Redirection to login page if not authentifiy fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'ArtSite') }}</title>
+    <title>ArtSite</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
